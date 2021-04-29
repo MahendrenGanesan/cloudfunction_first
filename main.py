286e6ec5b15d6d7eb009c6aba8d48051a112ab1f
Mahendren <ever.mahe@gmail.com>
2021-04-29 11:45:27 +0530

    code cleanup for better readabilty

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def test_function(request):
-    # Use a breakpoint in the code line below to debug your script.
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    # initial code starting from feature and added to release and finally added to main
     return 'Test Function Call'
 
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
